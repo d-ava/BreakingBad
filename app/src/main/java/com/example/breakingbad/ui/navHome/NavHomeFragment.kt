2149f81b5f68c7039fba22ed6d55303f6e008aa7
d-ava <d.avalishvili@gmail.com>
2022-03-26 02:38:42 +0400

    bottom navigation view

---
+++ app/src/main/java/com/example/breakingbad/ui/navHome/NavHomeFragment.kt
@@ -0,0 +1,11 @@
+package com.example.breakingbad.ui.navHome
+
+import androidx.fragment.app.Fragment
+import com.example.breakingbad.databinding.FragmentNavHomeBinding
+import com.example.breakingbad.ui.BaseFragment
+
+class NavHomeFragment : BaseFragment<FragmentNavHomeBinding>(FragmentNavHomeBinding::inflate) {
+    override fun start() {
+
+    }
+}
