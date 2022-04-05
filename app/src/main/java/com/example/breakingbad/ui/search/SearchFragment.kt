701bf289e9d91dd5941d766d63e9f464464bb1cb
d-ava <d.avalishvili@gmail.com>
2022-04-05 11:35:20 +0400

    search text works. when text field is empty, list is cleared

--- app/src/main/java/com/example/breakingbad/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/breakingbad/ui/search/SearchFragment.kt
@@ -1,13 +1,17 @@
 package com.example.breakingbad.ui.search
 
 import android.text.Editable
+import android.text.TextUtils
 import android.text.TextWatcher
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.breakingbad.R
 import com.example.breakingbad.databinding.FragmentSearchBinding
 import com.example.breakingbad.model.BBCharacter
 import com.example.breakingbad.ui.BaseFragment
+import com.example.breakingbad.ui.character.CharacterDetailsFragmentDirections
 import com.example.breakingbad.ui.home.BBAdapter
 import kotlinx.coroutines.flow.collect
 
@@ -37,37 +41,47 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(FragmentSearchBinding
 
     private fun setRecycler() {
         searchAdapter = BBAdapter {
-            //
+            val action = CharacterDetailsFragmentDirections.toCharacterDetailsFragment(it)
+            activity?.findNavController(R.id.mainContainer)?.navigate(action)
         }
 
         binding.recycler.adapter = searchAdapter
-        binding.recycler.layoutManager = GridLayoutManager(requireContext(), 3)
+        binding.recycler.layoutManager = GridLayoutManager(requireContext(), 2)
         searchAdapter.setData(emptyList)
     }
 
-    private fun search(){
-        fun filterText(text: String){
+    private fun search() {
+        fun filterText(text: String) {
             val filteredList = mutableListOf<BBCharacter>()
 
-            for (item in fullList){
-                if (item.name.lowercase().contains(text.lowercase())){
+
+
+            for (item in fullList) {
+                if (item.name.lowercase().contains(text.lowercase()) || item.nickname.lowercase()
+                        .contains(text.lowercase()) || item.portrayed.lowercase()
+                        .contains(text.lowercase())
+                ) {
                     filteredList.add(item)
                 }
             }
             searchAdapter.filteredList(filteredList)
         }
 
-        binding.etSearch.addTextChangedListener(object :TextWatcher{
+        binding.etSearch.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 //
             }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-               //
+
+                filterText(p0.toString())
             }
 
             override fun afterTextChanged(p0: Editable?) {
-                filterText(p0.toString())
+                if (TextUtils.isEmpty(p0.toString())){
+                    searchAdapter.setData(emptyList)
+                }
+//                filterText(p0.toString())
 
             }
         })
